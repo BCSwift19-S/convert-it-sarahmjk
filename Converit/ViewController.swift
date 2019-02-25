449d795477afb2c5e95fa1b59844cc75191a43e0
Sarah Kim <sarah.mjk@hotmail.com>
2019-02-25 00:45:17 -0500

    Initial Commit

---
+++ Converit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Converit
+//
+//  Created by Sarah Minji Kim on 2/25/19.
+//  Copyright © 2019 Sarah Minji Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
